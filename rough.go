f18454e6b63d7c649594ebcd3d25b9fbb9804a3f
kevit-shrut-sureja <shrut.sureja@kevit.io>
2024-09-30 18:35:15 +0530

    made some change here

--- rough.go
+++ rough.go
@@ -1,13 +1,40 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func main() {
+// 	// x := func () int {
+// 	// 	return 42
+// 	// }
+
+// 	// y := func () int {
+// 	// 	return 24
+// 	// }
+
+// 	// println(x(), y())
+
+// 	t := time.Now()
+
+// 	fmt.Printf("%T", t.UTC())
+// }
+
 package main
 
+import (
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	ans := make(map[string]int)
+	for _, char : range s {
+		m[char] += 1;
+	}
+	return m
+}
+
 func main() {
-	x := func () int {
-		return 42
-	}
-
-	y := func () int {
-		return 24
-	}
-
-	println(x(), y())
+	wc.Test(WordCount)
 }
